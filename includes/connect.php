6d9d139c9d7a36c9222f45d0c290750f5859f7a2
raiyan29 <78214519+raiyan29@users.noreply.github.com>
2021-04-13 13:45:27 +0530

    Added remote sql

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,15 @@
 <?php
 session_start();
-$servername = "localhost";
-$server_user = "root";
-$server_pass = "";
-$dbname = "food";
+//$servername = "localhost";
+//$server_user = "root";
+//$server_pass = "";
+//$dbname = "food";
+
+
+$servername = "remotemysql.com";
+$server_user = "Xw3xdpoYGt";
+$server_pass = "b0KqO0r8ie";
+$dbname = "Xw3xdpoYGt";
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 
